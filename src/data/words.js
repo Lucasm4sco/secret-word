0f26e58dd9160d0e174dbd55ad6133408502b630
Lucasm4sco <lmbp202@gmail.com>
2022-11-28 19:40:05 -0300

    first commit

---
+++ src/data/words.js
@@ -0,0 +1,20 @@
+export const wordsList = {
+    carro: [
+        "Motor", "Porta", "Capô", "Pneu", "Rádio", "Para-brisa", "Retrovisor", "Faróis", "Roda", "Volante", "Marcha"
+    ], 
+    fruta: [
+        "Banana", "Maçã", "Pera", "Mamão", "Laranja", "Limão", "Goiaba", "Jabuticaba", "Tomate", "Romã", "Jaca", "Ameixa"
+    ],
+    corpo: [
+        "Braço", "Perna", "Cérebro", "Pescoço", "Olhos", "Orelha", "Pé", "Unhas", "Cabelo", "Pele", "Orgãos", "Coração"
+    ],
+    computador: [
+        "Mouse", "Teclado", "Monitor", "Gabinete", "Memória", "Processador", "Carregador"
+    ],
+    programação: [
+        "Linguagem", "Framework", "React", "Javascript", "Front-end", "Back-end", "Desenvolvimento", "Tecnologias"
+    ],
+    alimento: [
+        "Arroz", "Macarrão", "Strogonoff", "Carne", "Leite", "Ovo", "Bolacha", "Pizza", "Salgado"
+    ]
+};
